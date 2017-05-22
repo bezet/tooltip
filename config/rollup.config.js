5687287dc0a2c91179d698b47117315539dd626e
bezet <bartosz.zadara@gmail.com>
2017-05-22 13:27:22 +0200

    Move rollup specific babel configuration to rollup config file

--- config/rollup.config.js
+++ config/rollup.config.js
@@ -10,6 +10,13 @@ export default {
     commonjs(),
     nodeResolve( { module: true, jsnext: true, main: true, browser: true } ),
     babel( {
+      babelrc: false,
+      "presets": [
+        ["env", {
+          "modules": false
+        }]
+      ],
+      "plugins": ["external-helpers"],
       exclude: 'node_modules/**'
     } )
   ],
