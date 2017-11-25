41b59cefe0e5d12c12bd87f0e6d29175378a7035
bezet <bartosz.zadara@gmail.com>
2017-11-25 23:34:56 +0100

    Set webpack configs

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -2,8 +2,5 @@ const merge = require('webpack-merge');
 const webpackConfig = require('./webpack.config.js');
 
 module.exports = merge(webpackConfig, {
-  devtool: 'eval-source-map',
-  plugins: [
-
-  ]
+  devtool: 'eval-source-map'
 });
